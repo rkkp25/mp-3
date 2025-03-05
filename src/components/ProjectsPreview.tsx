030bab94404704d71456c340a4288502e0ba929b
rkkp25 <rkkp@bu.edu>
2025-03-05 01:27:44 -0500

    trying to fix errors given by vercel p4

--- src/components/ProjectsPreview.tsx
+++ src/components/ProjectsPreview.tsx
@@ -167,10 +167,10 @@ export default function ProjectsPreview() {
                 value={input2}
                 onChange={(e) => setInput2(Number(e.target.value))} 
             />
-                <StyledButton onClick={setOperator("+")}>+</StyledButton>
-                <StyledButton onClick={setOperator("-")}>-</StyledButton>
-                <StyledButton onClick={setOperator("/")}>/</StyledButton>
-                <StyledButton onClick={setOperator("**")}>**</StyledButton>
+                <StyledButton onClick={(e) => setOperator(e.target.value)}>+</StyledButton>
+                <StyledButton onClick={(e) => setOperator(e.target.value)}>-</StyledButton>
+                <StyledButton onClick={(e) => setOperator(e.target.value)}>/</StyledButton>
+                <StyledButton onClick={(e) => setOperator(e.target.value)}>**</StyledButton>
                 <StyledButton onClick={calculate}>Calculate!</StyledButton>
                 <StyledP id="output2"></StyledP>
             </StyledDiv>
